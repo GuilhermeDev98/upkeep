969ba8ae360d30fe84e9000b7de7c2e741cd0c49
Guilherme Santos <guilhermedev@hotmail.com>
2023-01-09 15:27:07 -0300

    add .env file and alters image favicons

--- src/app/init.js
+++ src/app/init.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const initializeApp = () => {
     // Setting base URL for all API request via axios
-    axios.defaults.baseURL = 'http://localhost/api/v1/'
+    axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React,  { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -10,11 +10,11 @@ import SuspenseContent from './containers/SuspenseContent';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <Suspense fallback={<SuspenseContent />}>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </Suspense>
+  <Suspense fallback={<SuspenseContent />}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Suspense>
   // </React.StrictMode>
 );
 
